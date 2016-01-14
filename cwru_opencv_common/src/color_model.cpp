a754ec120f96ee767b5194b4fced11cbaffb0454
Russell Jackson <rcj33@case.edu>
2016-01-13 22:14:18 -0500

    Update color_model.cpp
    Adding the computational generation of the probability model.

--- cwru_opencv_common/src/color_model.cpp
+++ cwru_opencv_common/src/color_model.cpp
@@ -18,13 +18,21 @@ using cv::Mat;
 namespace cv_color_model{
 
 ColorModel::colorModel(const Mat & sourceImage, const cv::Mat maskImage){
-
+    // To obtain a full covariance matrix, use the calcCovar
+    
+    Mat colorStdDev;
+    meanStdDev(sourceImage, static_cast<Mat> (colorMean), (colorStdDev), maskImage);
+    void calcCovarMatrix(InputArray samples, OutputArray covar, OutputArray mean, int flags, int ctype=CV_64F);
+    //Generate the full data.
 }
 
 ColorModel::colorModel(const Mat & sourceImage){
     
 }
 
-cv::Mat ColorModel::segmentImage(Mat &); 
+cv::Mat ColorModel::segmentImage(Mat &inputImage){
+ 
+ 
+}
 
 }; //namespace cv_color_model
