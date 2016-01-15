794cc5d7674f171ee1347fa97bc5c82d8ec39446
Russell Jackson <rcj33@case.edu>
2016-01-15 11:42:33 -0500

    Update grab_cut.cpp
    Working on making a updateable grab cut object for segmentation

--- cwru_opencv_common/src/grab_cut.cpp
+++ cwru_opencv_common/src/grab_cut.cpp
@@ -46,35 +46,7 @@ static void getBinMask( const Mat& comMask, Mat& binMask )
     binMask = comMask & 1;
 }
 
-class GCApplication
-{
-public:
-    enum{ NOT_SET = 0, IN_PROCESS = 1, SET = 2 };
-    static const int radius = 2;
-    static const int thickness = -1;
 
-    void reset();
-    void setImageAndWinName( const Mat& _image, const string& _winName );
-    void showImage() const;
-    void mouseClick( int event, int x, int y, int flags, void* param );
-    int nextIter();
-    int getIterCount() const { return iterCount; }
-private:
-    void setRectInMask();
-    void setLblsInMask( int flags, Point p, bool isPr );
-
-    const string* winName;
-    const Mat* image;
-    Mat mask;
-    Mat bgdModel, fgdModel;
-
-    uchar rectState, lblsState, prLblsState;
-    bool isInitialized;
-
-    Rect rect;
-    vector<Point> fgdPxls, bgdPxls, prFgdPxls, prBgdPxls;
-    int iterCount;
-};
 
 void GCApplication::reset()
 {
